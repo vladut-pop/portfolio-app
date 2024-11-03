05d4fb59468d13d5df1119e7caf97520aa263373
Vlad Pop <vlad.pop@boatyardx.com>
2024-11-03 23:29:33 +0200

    Add @phosphor-icons/react dependency and update App component to use routing

--- src/pages/CV/CVPage.tsx
+++ src/pages/CV/CVPage.tsx
@@ -1,8 +1,27 @@
+import { data } from './data'
+import styles from './CVPage.module.css'
+import { At, GithubLogo } from '@phosphor-icons/react'
+
 const CVPage: React.FC = () => {
+  const { name, headline, location, picture, github, email } = data.basics
   return (
-    <div>
-      <h1>CVPage page</h1>
-      <p>Work in progress.</p>
+    <div className={styles.basics}>
+      <img src={picture} className={styles.basics__picture} />
+      <div>
+        <h1>{name}</h1>
+        <div>{headline}</div>
+        <address>{location}</address>
+        <div>
+          <div className={styles.basics__contact}>
+            <GithubLogo size={24} weight="thin" />
+            <a href={github}>{github}</a>
+          </div>
+          <div className={styles.basics__contact}>
+            <At size={24} weight="thin" />
+            <a href={`mailto:${email}`}>{email}</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/pages/CV/data.ts
@@ -0,0 +1,12 @@
+export const data = {
+  basics: {
+    github: 'https://vladut-pop.github.io/portfolio-app/',
+    name: 'Vlǎduț Traian POP',
+    email: 'vladpop@ucm.es',
+    phone: '',
+    picture:
+      'https://storage.rxresu.me/cm1v20mmu2ceqmtv50adxdxvw/pictures/cm1v20mmu2ceqmtv50adxdxvw.jpg',
+    headline: 'Frontend Developer',
+    location: 'Baia Mare, Romania',
+  },
+}
