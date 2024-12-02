0fa8bd5f0a9b1fb3508ac5d47ad5c9ae50f6801d
Vlad Pop <vlad.pop@boatyardx.com>
2024-12-02 14:45:16 +0200

    feat: increase the number of Block actors from 16 to 56; adjust their positions accordingly

--- src/game/Actors/actors.ts
+++ src/game/Actors/actors.ts
@@ -4,10 +4,9 @@ import { Hoodie } from './Hoodie'
 import carpet from '../../assets/images/blocks/Carpet_1.png'
 
 export const actors = [
-  ...Array(16)
+  ...Array(56)
     .fill('')
     .map((_, i) => Block({ position: { x: 64 * i, y: 64 * 8 }, src: carpet })),
-  Block({ position: { x: 128, y: 128 } }),
   Hoodie(),
   Bug(),
   // Instantiate other actors
